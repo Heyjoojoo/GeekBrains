99f7ee0c55d5eb16c391c60c0372d53d1acc21be
Marina <tiffuncile@yandex.ru>
2022-10-08 01:42:49 +0300

    Added task 2 to seminar 1 block 2

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar01/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+Console.Write("Понедельник");
+else if (n == 2)
+Console.Write("Вторник");
+else if (n == 3)
+Console.Write("Среда");
+else if (n == 4)
+Console.Write("Четверг");
+else if (n == 5)
+Console.Write("Пятница");
+else if (n == 6)
+Console.Write("Суббота");
+else if (n == 7)
+Console.Write("Воскресенье");
