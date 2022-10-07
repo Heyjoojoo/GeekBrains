547e1a67b6df61ef01a47640336779b16e0e5f21
Marina <tiffuncile@yandex.ru>
2022-10-08 01:41:09 +0300

    Added Task 1 to seminar 1 block 2

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar01/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b == a*a)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
