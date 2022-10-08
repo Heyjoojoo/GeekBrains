ee6a557bf61dac5f04e605597489d6ed0f5abd17
Marina <tiffuncile@yandex.ru>
2022-10-08 14:07:14 +0300

    Add task 001

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar03/Task001_XYcoordinates/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите координату х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у: ");
+double y = Convert.ToDouble(Console.ReadLine());
+while (x == 0)
+{
+    Console.Write("Введите координату х: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.Write("Введите координату y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
