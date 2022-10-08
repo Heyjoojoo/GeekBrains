758b55db97e5b942024eceed76fda10920e12fe3
Heyjoojoo <114143515+Heyjoojoo@users.noreply.github.com>
2022-10-08 14:10:23 +0300

    Delete 2ProgrammingLanguages/2SEMINARS/Seminar2 directory

--- 2ProgrammingLanguages/2SEMINARS/Seminar2/Task006_Problem4/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-if (n % 23 == 0 && n % 7 == 0)
-    Console.WriteLine($"Число {n} кратно 7 и 23");
-else
-    Console.WriteLine($"Число {n} не кратно 7 и 23");
--- 2ProgrammingLanguages/2SEMINARS/Seminar2/Task007_Problem5/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Введите два числа: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-if (n1*n1 == n2 || n2*n2 == n1)
-    Console.WriteLine("да");
-else
-    Console.WriteLine("нет");
