8fcbdb170146673545dc7e69dd13dd0fca873204
Marina <tiffuncile@yandex.ru>
2022-10-08 14:30:11 +0300

    Add Task 002

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar03/Task002_XYcoordinatesAsWell/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 && n > 4)
+{
+    Console.WriteLine("Неверный формат четверти, попробуйте снова: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1) // x > 0 && y > 0
+    Console.WriteLine("Координаты х и у больше 0");
+else if (n == 2) // х < 0 && y > 0
+    Console.WriteLine("Координаты х больше 0 и у меньше 0");
+else if (n == 3) //x < 0 && y < 0
+    Console.WriteLine("Координаты х и у меньше 0");
+else
+    Console.WriteLine("Координаты х меньше 0 и у больше 0");
