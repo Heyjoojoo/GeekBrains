270cb65330a80f94047c7d29874c0e4c01c9bf0d
Marina <tiffuncile@yandex.ru>
2022-10-08 15:07:06 +0300

    Added task 003

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar03/Task003_AB/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)), 3));
