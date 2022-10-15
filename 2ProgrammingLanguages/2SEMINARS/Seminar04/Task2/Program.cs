f2d35ba8e7ae5fa01d03c7498f35f2e8d901dfcc
Marina <tiffuncile@yandex.ru>
2022-10-15 13:21:20 +0300

    Added task 2 and 3

---
+++ 2ProgrammingLanguages/2SEMINARS/Seminar04/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (n > 0 )
+{
+    n /= 10;
+    result ++;
+} 
+
+Console.WriteLine(result+1);
